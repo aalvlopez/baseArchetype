c2e09ec85d2c387b7e564c36009af459fde32089
imhiringbot <84903970+imhiringbot@users.noreply.github.com>
2021-12-15 16:52:23 +0100

    Initial commit

---
+++ backend/taskmanagerFS/src/test/java/com/imatia/taskmanagerFS/TaskmanagerFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imatia.taskmanagerFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
