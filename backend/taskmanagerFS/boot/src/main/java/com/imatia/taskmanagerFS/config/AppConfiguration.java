f5201f2953de7df0ef4e535aad9494de5ccdda1c
Adolfo Alvarez <fito1001@gmail.com>
2021-12-18 09:48:12 +0100

    Configured backend base archetype and H2 DB

---
+++ backend/taskmanagerFS/boot/src/main/java/com/imatia/taskmanagerFS/config/AppConfiguration.java
@@ -0,0 +1,11 @@
+package com.imatia.taskmanagerFS.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author <a href="changeme@ext.inditex.com">aalvarez</a>
+ */
+@Configuration
+public class AppConfiguration {
+
+}
